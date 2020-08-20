092c809750aa1efd4a7f3de58ccf572dde5ca525
Cristian Osorio <cristian.osorio@globant.com>
2020-08-20 12:31:12 -0400

    version base del proyecto

---
+++ src/main/java/com/example/remotedemotechtalks/RemotedemotechtalksApplication.java
@@ -0,0 +1,13 @@
+package com.example.remotedemotechtalks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RemotedemotechtalksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RemotedemotechtalksApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/remotedemotechtalks/RemotedemotechtalksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.remotedemotechtalks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RemotedemotechtalksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
